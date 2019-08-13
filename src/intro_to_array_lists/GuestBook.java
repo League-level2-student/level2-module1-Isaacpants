4b1e6c65df847d044c0122c43469b22c7049f449
League Student <league@iMac-24.attlocal.net>
2019-08-13 16:19:39 -0700

    guestBook ip  ArrayList demo fin

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,7 +1,40 @@
 package intro_to_array_lists;
 
-public class GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GuestBook implements ActionListener{
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
+	public static void main(String[] args) {
+		GuestBook gb = new GuestBook();
+		gb.setup();
+		
+	}
+	void setup() {
+	JFrame jf = new JFrame();
+	JPanel jp = new JPanel();
+	JButton jb = new JButton();
+	JButton jb1 = new JButton();
+	
+	jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+	jf.setVisible(true);
+	jf.add(jp);
+	jp.add(jb);
+	jp.add(jb1);
+	
+	jb.setText("Add Name");
+	jb1.setText("View Names");
+	
+	
+	jb.addActionListener(this);
+	jb1.addActionListener(this);
+	
+	
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
 	// all the names added to the list. Format the list as follows:
@@ -9,4 +42,10 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
